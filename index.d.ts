23e231b26e5b266d3a295a1c0fe8d83696b30378
E0SelmY4V-PC <2507917746@qq.com>
2022-12-25 04:14:44 +0800

    Fix 声明错误

--- index.d.ts
+++ index.d.ts
@@ -3,5 +3,5 @@
  * @version 1.0.0
  * @link https://github.com/E0SelmY4V/schema2class
  */
-declare function _exports(schema: any): new () => any;
+declare function _exports(schema: any): new (n: any) => any;
 export = _exports;
