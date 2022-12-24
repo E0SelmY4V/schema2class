108c2bb5634b829094c3011f9397ec0c1bc82fb0
E0SelmY4V-PC <2507917746@qq.com>
2022-12-24 19:02:19 +0800

    Ref 整理

--- index.js
+++ index.js
@@ -1,90 +1,71 @@
-'use strict';
+"use strict";
 
 const getType = (n) => {
-	if (n === null) return 'null';
 	switch (typeof n) {
-		case 'bigint':
-		case 'number':
-			return 'number';
-		case 'boolean':
-			return 'bollean';
-		case 'object':
-			return Array.isArray(n) ? 'array' : 'object';
-		case 'string':
-			return 'string';
+		case 'bigint': case 'number': return 'number';
+		case 'boolean': return 'bollean';
+		case 'object': return n ? Array.isArray(n) ? 'array' : 'object' : 'null';
+		case 'string': return 'string';
+		default: return "null";
 	}
-	return "null";
 }
 
-this.getClsArr = () => {
+const getClsArr = () => {
 	function ParsedArr(n = null) {
 		const arr = [];
 		Object.setPrototypeOf(arr, ParsedArr.prototype);
-		arr.constructor = ParsedArr;
-		if (n === null) return arr;
-		if (typeof n == 'number') {
-			this.length = n;
-			return arr;
-		}
-		ParsedArr.prototype.eleClass
-			? n.forEach(e => this.push(new ParsedArr.prototype.eleClass(e)))
-			: arr.push(...n);
+		ParsedArr.prototype.constructor = ParsedArr;
+		n === null ? arr.length = ParsedArr.prototype.length
+			: typeof n === 'number' ? arr.length = n
+				: ParsedArr.prototype.__eleClass
+					? n.forEach(e => arr.push(new ParsedArr.prototype.__eleClass(e)))
+					: arr.push(...n);
 		return arr;
 	}
 	ParsedArr.prototype = [];
 	return ParsedArr;
 }
 
-this.getClsObj = () => function ParsedObj(n = null) {
-	if (n === null) return;
-	for (let i in n) typeof ParsedObj.prototype[i] !== 'object'
+const getClsObj = () => function ParsedObj(n = null) {
+	for (const i in n) typeof ParsedObj.prototype[i] !== 'object'
 		? this[i] = n[i]
 		: this[i] = new ParsedObj.prototype[i].constructor(n[i]);
 };
 
 const expArr = (schema) => {
-	let cls = this.getClsArr(), eleClass, func = e => cls.prototype.push(e);
+	const cls = getClsArr();
+	let func = e => cls.prototype.push(e);
 	switch (schema.items.type) {
-		case 'object':
-			eleClass = expObj(schema.items);
-			cls.prototype.eleClass = eleClass;
+		case 'object': {
+			const eleClass = expObj(schema.items);
+			cls.prototype.__eleClass = eleClass;
 			func = e => cls.prototype.push(new eleClass(e));
-			break;
-		case 'array':
-			eleClass = expArr(schema.items);
-			cls.prototype.eleClass = eleClass;
+		} break;
+		case 'array': {
+			const eleClass = expArr(schema.items);
+			cls.prototype.__eleClass = eleClass;
 			func = e => {
 				let ins = new eleClass(e);
 				cls.prototype.push(ins);
 				ins.length = eleClass.prototype.length;
 			}
-			break;
+		} break;
 	}
-	schema.default.forEach(func);
-	return cls;
+	return (schema.default || []).forEach(func), cls;
 }
 
 const expObj = (schema) => {
-	let cls = this.getClsObj();
+	const cls = getClsObj();
 	for (let i in schema.properties) {
 		let attr = schema.properties[i], type = attr.type;
-		if (typeof type !== 'string') {
-			type = typeof attr.default === 'undefined' ? type[0] : getType(attr.default);
-		}
+		if (typeof type !== 'string') type = typeof attr.default === 'undefined' ? type[0] : getType(attr.default);
 		switch (type) {
-			case 'object':
-				cls.prototype[i] = new (expObj(attr));
-				break;
-			case 'array':
-				cls.prototype[i] = new (expArr(attr));
-				cls.prototype[i].length = Object.getPrototypeOf(cls.prototype[i]).length;
-				break;
-			default:
-				cls.prototype[i] = attr.default;
-				break;
+			case 'object': cls.prototype[i] = new (expObj(attr)); break;
+			case 'array': cls.prototype[i] = new (expArr(attr)); break;
+			default: cls.prototype[i] = attr.default; break;
 		}
 	}
-	return cls;
+	return schema.default && (cls.prototype = new cls(schema.default)), cls;
 }
 
-module.exports = (schema) => (Array.isArray(schema) ? expArr : typeof schema === 'object' ? expObj : _ => _)(schema);
+module.exports = (schema) => Array.isArray(schema) ? expArr(schema) : typeof schema === 'object' ? expObj(schema) : schema.constructor;
