0ff6d017c7a1d7651f46dbe57c47766b733ccb63
E0SelmY4V-PC <2507917746@qq.com>
2023-01-15 15:29:54 +0800

    Add 详细声明

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,18 @@
+import type { OfSchema } from 'accurtype'
+type AllPartial<T> = { [I in keyof T]?: AllPartial<T[I]>; }
 /**
  * JSON Schema Parser
  * @version 2.5.2
  * @link https://github.com/E0SelmY4V/schema2class
  */
 declare const _exports: {
-	(schema?: any, option?: _exports.ParseOption): (n?: any) => any;
+	<T>(schema?: T, option?: _exports.ParseOption): (n?: AllPartial<OfSchema<T>>) => OfSchema<T>;
 	realArray: boolean;
 	check: boolean;
 	checkKey: boolean;
 	checkType: boolean;
-};
-export = _exports;
+}
+export = _exports
 declare namespace _exports {
 	type ParseOption = Partial<typeof _exports>
 }
